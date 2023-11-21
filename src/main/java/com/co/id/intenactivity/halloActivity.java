78f8722ad7a70cd64ae3cd05f31cfa2c3e44c422
MalikAziz12 <Malikaziz12>
2023-11-22 01:11:37 +0700

    IntenActivity

---
+++ src/main/java/com/co/id/intenactivity/halloActivity.java
@@ -0,0 +1,14 @@
+package com.co.id.intenactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class halloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo);
+    }
+}
---
+++ src/main/java/com/co/id/intenactivity/scrollingTextActivity.java
@@ -0,0 +1,14 @@
+package com.co.id.intenactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scrollingTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling_text);
+    }
+}
